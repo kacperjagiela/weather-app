f7adb783a44f6a272befcc00b75d7bd3c778c1b4
kacperjagiela <jagielakacper80@gmail.com>
2019-08-25 17:29:42 +0200

    Updates

---
+++ precache-manifest.16de1e7a7623e38d3457da00e9133a1a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a7fb3e2d54debe41cce8131a2e7ec50",
+    "url": "/weather-app/index.html"
+  },
+  {
+    "revision": "65dd7f11b7230060177c",
+    "url": "/weather-app/static/css/2.e8d046a2.chunk.css"
+  },
+  {
+    "revision": "7858c854a8b18b53d591",
+    "url": "/weather-app/static/css/main.72b12d0b.chunk.css"
+  },
+  {
+    "revision": "65dd7f11b7230060177c",
+    "url": "/weather-app/static/js/2.c1e9bac9.chunk.js"
+  },
+  {
+    "revision": "7858c854a8b18b53d591",
+    "url": "/weather-app/static/js/main.1db18247.chunk.js"
+  },
+  {
+    "revision": "7a390137f1e79ffd409f",
+    "url": "/weather-app/static/js/runtime~main.caa3d5bf.js"
+  }
+]);
